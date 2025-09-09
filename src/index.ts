f6a468e9d909d6d38193ee4e8c2ed66ec58eb5de
一刻相册 <china@aliyun.com>
2025-09-10 06:04:16 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import { R2Explorer } from "r2-explorer";
+import { R2Explorer } from 'r2-explorer';
 
-export default R2Explorer({
-  // Set to false to allow users to upload files
-  readonly: true,
+export default R2Explorer({ readonly: false }
 
   // Learn more how to secure your R2 Explorer instance:
   // https://r2explorer.com/getting-started/security/
